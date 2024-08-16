89faf93cf3b67f2bf20b6557664d37134768d543
Bellfalasch <bellfalasch@gmail.com>
2024-08-16 13:25:38 +0200

    fix: more notes

--- app/page.tsx
+++ app/page.tsx
@@ -3,111 +3,89 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1>X-mas Beer App</h1>
+      <h2>User journey</h2>
+      <ul>
+        <li>Login (using auth0)</li>
+        <li>From a DB, pick one event</li>
+        <li>
+          Event contains rules for voting (categories, available points, and a
+          list of beers)
+          <ul>
+            <li>Admin for events can be pure DB inserts</li>
+            <li>Add 1 to X categories, like Taste, Color, Design, whatnot</li>
+            <li>
+              For each category, define the available points, 1 to 6 being
+              default
+            </li>
+            <li>
+              Now, each event need to either be &quot;Add as you go&quot; or
+              &quot;List of beer&quot;-type
+            </li>
+            <li>
+              &quot;Add as you go&quot; means when an event starts, a beer must
+              be added to the pool before anyone can vote on it
+            </li>
+            <li>
+              &quot;List of beer&quot; means all beers are added before the
+              event starts, stepping from first to last
+            </li>
+          </ul>
+        </li>
+        <li>
+          As a logged in user, you will receive either the first beer of the
+          list, or be forced to add one
+        </li>
+        <li>
+          When voting, the users sees all available categories and the points
+          available
+        </li>
+        <li>The users clicks &quot;Vote&quot; when ready</li>
+        <li>All users can see &quot;X of X votes sent in</li>
+        <li>
+          When all votes are done, all users get a new button: &quot;Ready for
+          next beer&quot;
+        </li>
+        <li>
+          Until everyone have said that they are ready, each user see a summary
+          of their given points on the given beer
+        </li>
+        <li>When everyone are done, next beer comes up (or the event stops)</li>
+      </ul>
+      <h2>Functionality</h2>
+      <ul>
+        <li>
+          Login using
+          [Auth0?](https://auth0.com/docs/quickstart/webapp/nextjs/interactive)
+        </li>
+        <li>
+          For each beer fetch image and data from [Untappd
+          API](https://untappd.com/api/docs#beerinfo)?
+        </li>
+        <li>
+          A user should be able to edit points for a beer until they click ready
+          for next beer.
+        </li>
+        <li>
+          Based on total votes, we should be able to fetch some stats, like
+          wirst/best for each category, total win/loss, etc.
+        </li>
+        <li>
+          The app should handle people joining late, and people leaving early.
+        </li>
+        <li>
+          The app should handle a way to re-pick next beer if everyone is
+          unhappy with the current one. Let say we get five very strong beers in
+          a row, it would be nice to be able to control that in a way.
+        </li>
+        <li>
+          If event has a list of beers, we could select next one randomly, and
+          just have a button to fetch another one, or we could let users vote
+          from the entire list of beers, or just let one person per round select
+          from the list. &quot;Next beer&quot; could thus be very simple, or
+          very complicated.
+        </li>
+      </ul>
     </main>
   );
 }
