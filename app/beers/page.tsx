bad77ec06de3009081c40f0865675f7002d52343
Bellfalasch <bellfalasch@gmail.com>
2024-09-03 12:34:08 +0200

    feat: do count and avg in SQL and display in a table

--- app/beers/page.tsx
+++ app/beers/page.tsx
@@ -1,18 +1,43 @@
 import { sql } from "@vercel/postgres";
 
 async function Beers(): Promise<JSX.Element> {
-  const { rows } = await sql`SELECT * FROM beers ORDER BY id`;
+  const { rows } =
+    await sql`SELECT b.id, b.name, b.description, COUNT(v.*) AS votes, AVG(v.points_taste) AS taste, AVG(v.points_design) AS design, AVG(v.points_bonus) AS bonus FROM beers b LEFT OUTER JOIN votes v ON v.beer_id = b.id GROUP BY b.id ORDER BY b.id`;
 
-  return (
-    <div>
+return (
+  <table>
+    <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Description</th>
+      <th>Votes</th>
+      <th>Taste</th>
+      <th>Design</th>
+      <th>Bonus</th>
+    </thead>
+    <tbody>
       {rows.map((row) => (
-        <div key={row.id}>
-          {row.id} - {row.name} - {row.description}
-        </div>
+        <tr key={row.id}>
+          <td>{row.id}</td>
+          <td>{row.name}</td>
+          <td>{row.description}</td>
+          <td>{row.votes}</td>
+          <td>
+            {row.taste ? parseFloat(row.taste.toString()).toFixed(1) : "-"}
+          </td>
+          <td>
+            {row.design ? parseFloat(row.design.toString()).toFixed(1) : "-"}
+          </td>
+          <td>
+            {row.bonus ? parseFloat(row.bonus.toString()).toFixed(1) : "-"}
+          </td>
+        </tr>
       ))}
-    </div>
-  );
+    </tbody>
+  </table>
+);
 }
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
