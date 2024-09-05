53ab43f25e1b44fb75b7df661aed3ac7e4dd36b2
Bellfalasch <bellfalasch@gmail.com>
2024-09-05 12:53:57 +0200

    feat: add a dropdown for available beers when voting

--- app/votes/page.tsx
+++ app/votes/page.tsx
@@ -1,38 +1,48 @@
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import addVote from "@/db/votes/vote";
+import listBeers from "@/db/beers/get-all";
 import React from "react";
+import { Beer } from "@/types";
+
 
 const submitVote = async (formData: FormData) => {
-  "use server"; // This runs on the server, so console.logs here and deeper will not be in the browser console ;P
-
-  const castVote = await addVote(
-    1,
-    1,
-    parseInt(formData.get("beer") as string, 10),
-    parseInt(formData.get("taste") as string, 10),
-    parseInt(formData.get("design") as string, 10),
-    parseInt(formData.get("bonus") as string, 10)
-  );
-
-  console.log("Added vote", castVote);
-
-  revalidatePath("/votes");
-  redirect(`/votes`);
+    "use server"; // This runs on the server, so console.logs here and deeper will not be in the browser console ;P
+    
+    const castVote = await addVote(
+        1,
+        1,
+        parseInt(formData.get("beer") as string, 10),
+        parseInt(formData.get("taste") as string, 10),
+        parseInt(formData.get("design") as string, 10),
+        parseInt(formData.get("bonus") as string, 10)
+    );
+    
+    console.log("Added vote", castVote);
+    
+    revalidatePath("/votes");
+    redirect(`/votes`);
 };
 
-export default function AddVote() {
+async function getAllBeers() {
+  const beers = await listBeers();
+  return !beers ? [] : beers;
+}
+
+export default async function AddVote() {
+  const allBeers = await getAllBeers();
+
   return (
     <form action={submitVote} className="flex flex-col place-items-baseline">
       <h1 className="text-xl mb-2">Add a Vote</h1>
       <label htmlFor="fieldBeer">Beer:</label>
-      <input
-        name="beer"
-        id="fieldBeer"
-        type="text"
-        required
-        className="border"
-      />
+      <select name="beer" id="fieldBeer">
+        {allBeers?.map((beer: Beer) => (
+          <option key={beer.id} value={beer.id}>
+            {beer.name}
+          </option>
+        ))}
+      </select>
 
       <fieldset>
         <legend>Points for taste (0-6):</legend>
@@ -50,30 +60,30 @@ export default function AddVote() {
         </label>
         <label>
           <input name="taste" type="radio" required value={2} /> 2 points
-        </label> 
-        <label>
-            <input name="taste" type="radio" required value={2.5} /> 2.5 points
         </label>
         <label>
-            <input name="taste" type="radio" required value={3} /> 3 points
+          <input name="taste" type="radio" required value={2.5} /> 2.5 points
         </label>
         <label>
-            <input name="taste" type="radio" required value={3.5} /> 3.5 points
+          <input name="taste" type="radio" required value={3} /> 3 points
         </label>
         <label>
-            <input name="taste" type="radio" required value={4} /> 4 points
+          <input name="taste" type="radio" required value={3.5} /> 3.5 points
         </label>
         <label>
-            <input name="taste" type="radio" required value={4.5} /> 4.5 points
+          <input name="taste" type="radio" required value={4} /> 4 points
         </label>
         <label>
-            <input name="taste" type="radio" required value={5} /> 5 points
+          <input name="taste" type="radio" required value={4.5} /> 4.5 points
         </label>
         <label>
-            <input name="taste" type="radio" required value={5.5} /> 5.5 points
+          <input name="taste" type="radio" required value={5} /> 5 points
         </label>
         <label>
-            <input name="taste" type="radio" required value={6} /> 6 points
+          <input name="taste" type="radio" required value={5.5} /> 5.5 points
+        </label>
+        <label>
+          <input name="taste" type="radio" required value={6} /> 6 points
         </label>
       </fieldset>
 
@@ -93,30 +103,30 @@ export default function AddVote() {
         </label>
         <label>
           <input name="design" type="radio" required value={2} /> 2 points
-        </label> 
-        <label>
-            <input name="design" type="radio" required value={2.5} /> 2.5 points
         </label>
         <label>
-            <input name="design" type="radio" required value={3} /> 3 points
+          <input name="design" type="radio" required value={2.5} /> 2.5 points
         </label>
         <label>
-            <input name="design" type="radio" required value={3.5} /> 3.5 points
+          <input name="design" type="radio" required value={3} /> 3 points
         </label>
         <label>
-            <input name="design" type="radio" required value={4} /> 4 points
+          <input name="design" type="radio" required value={3.5} /> 3.5 points
         </label>
         <label>
-            <input name="design" type="radio" required value={4.5} /> 4.5 points
+          <input name="design" type="radio" required value={4} /> 4 points
         </label>
         <label>
-            <input name="design" type="radio" required value={5} /> 5 points
+          <input name="design" type="radio" required value={4.5} /> 4.5 points
         </label>
         <label>
-            <input name="design" type="radio" required value={5.5} /> 5.5 points
+          <input name="design" type="radio" required value={5} /> 5 points
         </label>
         <label>
-            <input name="design" type="radio" required value={6} /> 6 points
+          <input name="design" type="radio" required value={5.5} /> 5.5 points
+        </label>
+        <label>
+          <input name="design" type="radio" required value={6} /> 6 points
         </label>
       </fieldset>
 
@@ -136,12 +146,12 @@ export default function AddVote() {
         </label>
         <label>
           <input name="bonus" type="radio" required value={2} /> 2 points
-        </label> 
-        <label>
-            <input name="bonus" type="radio" required value={2.5} /> 2.5 points
         </label>
         <label>
-            <input name="bonus" type="radio" required value={3} /> 3 points
+          <input name="bonus" type="radio" required value={2.5} /> 2.5 points
+        </label>
+        <label>
+          <input name="bonus" type="radio" required value={3} /> 3 points
         </label>
       </fieldset>
 
